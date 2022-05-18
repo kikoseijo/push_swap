42949765ddfff75a8e3a78bff863f6f8f956d22e
jseijo-p <jseijo-p@student.42malaga.com>
2022-05-18 10:08:07 +0200

    (int)Array funtions.

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 19:09:04 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/05/18 09:27:57 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/05/18 09:53:39 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,24 @@
 
 # include <stdio.h>
 
-typedef struct s_stack
+typedef struct s_model
 {
-	int	*vals;
-	int	next;
+	int		**list_a;
+	int		**list_b;
+	int		max_a;
+	int		min_a;
+	int		max_b;
+	int		min_b;
+	char	**operations;
+	int		next;
 
-}		t_stack;
+}			t_model;
+
+/*
+** array_functions
+*/
+
+void		print_array(int **arr);
+int			find_element_index(int *arr, int el);
 
 #endif
---
+++ src/array_functions.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   array_functions.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseijo-p <jseijo-p@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 09:44:04 by jseijo-p          #+#    #+#             */
+/*   Updated: 2022/05/18 10:07:31 by jseijo-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	print_array(int *arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		ft_putnbr_fd(arr[i], 1);
+		ft_putchar_fd('\n', 1);
+		i++;
+	}
+}
+
+int	find_element_index(int *arr, int el)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		if (arr[i] == el)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	insert_element_at_index(int *arr, int el, int index)
+{
+	int	capacity;
+
+	capacity = (sizeof(*arr) / sizeof(arr[0]));
+	if (index >= capacity)
+		return (index);
+	arr[index] = el;
+	return (index + 1);
+}
+
+int	delete_element(int *arr, int el)
+{
+	int	position;
+
+	position = find_element_index(arr, el);
+	if (position == -1)
+		return (-1);
+	while (arr[position])
+	{
+		arr[position] = arr[position + 1];
+		position++;
+	}
+}
